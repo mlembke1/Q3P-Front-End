456be0cec6d93cc49ffaacc43560cf039e6e3abc
Nathan Burgo <nthnbrg185@gmail.com>
2018-06-02 09:00:29 -0600

    Small styling changes to homepage to see what everything looks like on ios

--- App.js
+++ App.js
@@ -11,11 +11,24 @@ import {
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello software interview candidates.</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello software interview candidates.</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 80
+  },
+  text: {
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignContent: 'flex-start'
+  }
+})
 
 AppRegistry.registerComponent('App', () => App);
