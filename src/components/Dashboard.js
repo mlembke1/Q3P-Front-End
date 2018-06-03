d8ce05d6bf9aacc9c69ec98601e825be1cfa9d76
Nathan Burgo <nthnbrg185@gmail.com>
2018-06-02 23:19:21 -0600

    Heavy work on dashboard formatting

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,41 +6,106 @@ import {
   View,
   AppRegistry,
   ScrollView,
-  TextInput
+  TextInput,
+  Dimensions,
+  ImageBackground,
+  StatusBar
 } from 'react-native'
-import { Button } from 'react-native-elements'
+import { Button, Card, Icon } from 'react-native-elements'
+
+const MyStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+)
+
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text
-          style={styles.text}>
-          Welcome
-        </Text>
-        <View style={styles.buttonContainer} />
-        <Button
-          onPress={this.props.onLogoutPress}
-          title="Logout"
-         />
-      </ScrollView>
+      <ImageBackground source={require('../../assets/background.jpg')} style={styles.image}>
+        <View style={styles.statusContainer}>
+          <MyStatusBar backgroundColor="#79B45D" barStyle="light-content" />
+          <View style={styles.appBar}>
+            <Icon name="dots-three-horizontal" />
+          </View>
+        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text style={styles.text}>
+            My Dashboard
+          </Text>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>
+              View Decks
+            </Text>
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>
+              View Decks
+            </Text>
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>
+              View Decks
+            </Text>
+          </View>
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>
+              View Decks
+            </Text>
+          </View>
+          <View style={styles.buttonContainer} />
+          <Button
+            onPress={this.props.onLogoutPress}
+            title="Logout"
+           />
+        </ScrollView>
+      </ImageBackground>
     )
   }
 }
 
+const width = Dimensions.get('window').width
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: 200,
-    padding: 20
+    display: 'flex',
+    flex: 1,
+    padding: 20,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  card: {
+    width: (width / 2) - 40,
+    height: (width / 2) - 40,
+    margin: 10,
+    justifyContent: 'center',
+    backgroundColor: 'white'
   },
   text: {
     fontFamily: 'Arial',
     fontWeight: 'bold',
     fontSize: 27,
+    color: 'white'
+  },
+  cardTitle: {
     textAlign: 'center'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    opacity: 10
   },
   buttonContainer: {
     margin: 20
+  },
+  statusBar: {
+    height: 20,
+  },
+  appBar: {
+    backgroundColor: '#79B45D',
+    height: 40,
   }
 })
 
