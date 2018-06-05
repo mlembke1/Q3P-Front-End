b9e5472086035fcf7896f46579ea014a357dc8ee
Torrey Smith <torreypsmith@aol.com>
2018-06-05 17:17:56 -0600

    dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -16,91 +16,49 @@ import { Button, Card } from 'react-native-elements'
 import { createStackNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { onSignOut } from "./Auth"
-import axios from 'axios'
-import { REACT_APP_API_URL } from 'react-native-dotenv'
 
-export default class Dashboard extends Component {
-
-  constructor(){
-    super()
-    this.state = {
-      userDecks: [],
-      publicDecks: []
-    }
-  }
-
-  componentDidMount(){
-      this.fetchAllUserDecks()
-      this.fetchAllPublicDecks()
-  }
-
-  fetchAllUserDecks(){
-    axios.get(`${REACT_APP_API_URL}/getAllDecksForUser`)
-    .then(r => {
-      this.setState({
-        userDecks: r.data.userDecks
-      })
-    })
-    .catch(err => console.log(err))
-  }
-
-  fetchAllPublicDecks(){
-    axios.get(`${REACT_APP_API_URL}/getAllPublicDecks`)
-    .then(r => {
-      this.setState({
-        publicDecks: r.data.publicDecks
-      })
-    })
-    .catch(err => console.log(err))
-  }
-
-
-  render(){
-    return(
-      <View style={styles.background}>
-        <ScrollView contentContainerStyle={styles.container}>
-          <StatusBar barStyle="light-content" />
-        <TouchableHighlight onPress={() => this.props.navigation.navigate('Decks')}>
-            <View style={[styles.card, { backgroundColor: '#b4645d' }]}>
-              <Icon style={styles.cardIcon} name="folder-open" color="white" size={28} />
-              <Text style={styles.cardTitle}>
-                VIEW DECKS
-              </Text>
-            </View>
-          </TouchableHighlight>
-          <View style={[styles.card, { backgroundColor: '#b45da4' }]}>
-            <Icon style={styles.cardIcon} name="plus-square" color="white" size={28} />
-            <Text style={styles.cardTitle}>
-              CREATE NEW DECK
-            </Text>
-          </View>
-          <View style={[styles.card, { backgroundColor: '#995db4'}]}>
-            <Icon style={styles.cardIcon} name="users" color="white" size={28} />
-            <Text style={styles.cardTitle}>
-              SOCIAL
-            </Text>
-          </View>
-          <View style={[styles.card, { backgroundColor: '#5d96b4' }]}>
-            <Icon style={styles.cardIcon} name="user-circle" color="white" size={28} />
-            <Text style={styles.cardTitle}>
-              PROFILE
-            </Text>
-          </View>
-          <Button
-            backgroundColor="#03A9F4"
-            title="LOGOUT"
-            onPress={() => {
-                onSignOut().then(() => this.props.navigation.navigate("SignedOut"))
-                axios.get(`${REACT_APP_API_URL}/logout`)
-                .then(r => console.log(r))
-                .then(err => console.log(err))
-            }}
-          />
-        </ScrollView>
+export default ({ navigation }) => (
+  <View style={styles.background}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.profile}>
+        <Text style={styles.username}>John Doe</Text>
+        <View
+          style={styles.imagePlaceholder}>
+          <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
+        </View>
       </View>
-    )
-  }
-}
+      <TouchableHighlight onPress={() => navigation.navigate('Decks')}>
+        <View style={[styles.card, { backgroundColor: '#b4645d' }]}>
+          <Icon style={styles.cardIcon} name="folder-open" color="white" size={34} />
+          <Text style={styles.cardTitle}>
+            VIEW DECKS
+          </Text>
+        </View>
+      </TouchableHighlight>
+      <View style={[styles.card, { backgroundColor: '#b45da4' }]}>
+        <Icon style={styles.cardIcon} name="plus-square" color="white" size={34} />
+        <Text style={styles.cardTitle}>
+          CREATE DECK
+        </Text>
+      </View>
+      <View style={[styles.card, { backgroundColor: '#995db4'}]}>
+        <Icon style={styles.cardIcon} name="users" color="white" size={34} />
+        <Text style={styles.cardTitle}>
+          SOCIAL
+        </Text>
+      </View>
+      <TouchableHighlight onPress={() => navigation.navigate('Settings')}>
+      <View style={[styles.card, { backgroundColor: '#5d96b4' }]}>
+        <Icon style={styles.cardIcon} name="gear" color="white" size={34} />
+        <Text style={styles.cardTitle}>
+          SETTINGS
+        </Text>
+      </View>
+    </TouchableHighlight>
+    </ScrollView>
+  </View>
+)
 
 const width = Dimensions.get('window').width
 
