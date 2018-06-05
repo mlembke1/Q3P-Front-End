5afa0576898ea8afa3aba459b94c4676d2a2934b
Torrey Smith <torreypsmith@aol.com>
2018-06-05 17:11:26 -0600

    created social branch

---
+++ src/components/Social.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  ScrollView,
+  TextInput,
+  Image,
+  ImageBackground
+} from 'react-native'
+import { Card, Button, FormLabel, FormInput } from "react-native-elements"
+
+export default class Social extends Component {
+
+  render(){
+    return (
+      <ScrollView>
+        <View>
+          SOCIAL
+        </View>
+      </ScrollView>
+    )
+  }
+}
