552dcbea7e91935c3ecdc8291d4baa89521aac2f
Torrey Smith <torreypsmith@aol.com>
2018-06-06 14:51:34 -0600

    public page searches

--- src/components/Public.js
+++ src/components/Public.js
@@ -18,42 +18,88 @@ import Dashboard from './Dashboard'
 
 
 export default class Public extends Component {
+ constructor(props){
+   super(props)
+     this.state = ({
+       searchDecks: []
+     })
+ }
+
+searchInput = () => {
+  console.log(this.refs.input.input._lastNativeText)
+  let searchedDecks = this.props.navigation.state.params.publicDecks.filter(deck =>
+  deck.title === this.refs.input.input._lastNativeText ||
+  deck.author === this.refs.input.input._lastNativeText ||
+  deck.subject === this.refs.input.input._lastNativeText)
+  this.setState({
+    searchDecks: searchedDecks
+  })
+}
+
   render(){
     return(
       <View style={styles.container}>
-        <View>
+        <View style={styles.searchContainer}>
+          <TouchableHighlight underlayColor="transparent" activeOpacity={0.5} onPress={this.searchInput}>
+            <View style={styles.searchButton}>
+              <Icon name="search" color="white" size={30}></Icon>
+            </View>
+          </TouchableHighlight>
           <SearchBar
             lightTheme
-            showLoading
+            showLoading={true}
             platform="ios"
-            cancelButtonTitle="Cancel"
+            ref="input"
+            noIcon={true}
             placeholder='Search'
-            containerStyle={styles.searchContainer}
             inputStyle={styles.searchText}/>
         </View>
-        <ScrollView>
-          {this.props.navigation.state.params.publicDecks.map(deck =>
-          <TouchableHighlight key={deck.id} underlayColor="transparent" activeOpacity={0.5} onPress={() => console.log("see deck #", deck.id)}>
-            <View style={styles.decks}>
-              <View style={styles.subjectAuthorContainer}>
-                <View style={styles.titleContainer}>
-                  <Text style={styles.titleText}>{deck.title}</Text>
+        <View>
+          <ScrollView style={{ marginBottom: 75}}>
+            {this.state.searchDecks.length < 1 ? this.props.navigation.state.params.publicDecks.map(deck =>
+            <TouchableHighlight key={deck.id} underlayColor="transparent" activeOpacity={0.5} onPress={() => console.log("see deck #", deck.id)}>
+              <View style={styles.decks}>
+                <View style={styles.subjectAuthorContainer}>
+                  <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>{deck.title}</Text>
+                  </View>
+                  <View style={styles.authorContainer}>
+                    <Icon style={styles.arrowStyle} name="chevron-circle-right" size={34}></Icon>
+                  </View>
                 </View>
-                <View style={styles.authorContainer}>
-                  <Icon style={styles.arrowStyle} name="chevron-circle-right" size={34}></Icon>
+                <View style={styles.subjectAuthorContainer}>
+                  <View>
+                    <Text style={styles.subjectText}>{deck.subject}</Text>
+                  </View>
+                  <View style={styles.authorContainer}>
+                    <Text style={styles.authorText}>{deck.author}</Text>
+                  </View>
                 </View>
               </View>
-              <View style={styles.subjectAuthorContainer}>
-                <View>
-                  <Text style={styles.subjectText}>{deck.subject}</Text>
+            </TouchableHighlight>) :
+            this.state.searchDecks.map(deck =>
+            <TouchableHighlight key={deck.id} underlayColor="transparent" activeOpacity={0.5} onPress={() => console.log("see deck #", deck.id)}>
+              <View style={styles.decks}>
+                <View style={styles.subjectAuthorContainer}>
+                  <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>{deck.title}</Text>
+                  </View>
+                  <View style={styles.authorContainer}>
+                    <Icon style={styles.arrowStyle} name="chevron-circle-right" size={34}></Icon>
+                  </View>
                 </View>
-                <View style={styles.authorContainer}>
-                  <Text style={styles.authorText}>{deck.author}</Text>
+                <View style={styles.subjectAuthorContainer}>
+                  <View>
+                    <Text style={styles.subjectText}>{deck.subject}</Text>
+                  </View>
+                  <View style={styles.authorContainer}>
+                    <Text style={styles.authorText}>{deck.author}</Text>
+                  </View>
                 </View>
               </View>
-            </View>
-          </TouchableHighlight>)}
-        </ScrollView>
+            </TouchableHighlight>)}
+          </ScrollView>
+        </View>
       </View>
       )
   }
@@ -63,18 +109,39 @@ const width = Dimensions.get('window').width
 
 const styles = StyleSheet.create({
   searchContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingBottom: 70
+  },
+  searchButton: {
+    marginTop: 8,
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 5,
+    backgroundColor: '#79B45D',
+    padding: 5,
+    paddingRight: 9,
+    paddingLeft: 9,
+    borderRadius: 5,
     height: 50,
-    marginTop: 5,
-    marginBottom: 5,
+    shadowColor: 'black',
+    shadowOffset: { width: 3, height: 3 },
+    shadowRadius: 3,
+    shadowOpacity: .7
   },
   container: {
     paddingLeft: 5,
     paddingRight:1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: 35
   },
   searchText: {
     fontSize: 20,
     color: 'black',
+    height: 50,
+    width: width-90
   },
   titleText: {
     fontSize: 26,
